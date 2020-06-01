216e0b260b45a5ff8d748a631ee4d33b3a574128
bespontoff <bespontoff@gmail.com>
2020-06-02 02:33:26 +0300

    хендлер кнопок с темами

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ proxies = {
     # 'http': 'http://163.172.182.180:1234',
     'https': 'https://163.172.182.180:1234',
 }
-
+#TODO: автоматическое сканирование прокси и менеджер с реконектами
 
 class JokeBot:
     about_text = 'JokeBot v.0.1'
@@ -23,20 +23,20 @@ class JokeBot:
             apihelper.proxy = proxy
 
         self.themes = {
-            1: 'Анекдоты',
-            2: 'Рассказы',
-            3: 'Стишки',
-            4: 'Афоризмы',
-            5: 'Цитаты',
-            6: 'Тосты',
-            8: 'Статусы',
-            11: 'Анекдоты (+18)',
-            12: 'Рассказы (+18)',
-            13: 'Стишки (+18)',
-            14: 'Афоризмы (+18)',
-            15: 'Цитаты (+18)',
-            16: 'Тосты (+18)',
-            18: 'Статусы (+18)',
+            'Анекдоты': 1,
+            'Рассказы': 2,
+            'Стишки': 3,
+            'Афоризмы': 4,
+            'Цитаты': 5,
+            'Тосты': 6,
+            'Статусы': 8,
+            'Анекдоты (+18)': 11,
+            'Рассказы (+18)': 12,
+            'Стишки (+18)': 13,
+            'Афоризмы (+18)': 14,
+            'Цитаты (+18)': 15,
+            'Тосты (+18)': 16,
+            'Статусы (+18)': 18,
         }
 
     def setup_handlers(self):
@@ -45,17 +45,30 @@ class JokeBot:
 
         @self.bot.message_handler(commands=['start', 'help'])
         def handle_start_help(message):
-            buttons = self.themes.values()
+            self.logger.debug(message)
+
+            buttons = self.themes.keys()
             markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
             for button in buttons:
                 markup.row(button)
             self.bot.send_message(message.chat.id, JokeBot.about_text, reply_markup=markup)
+            self.logger.info(f'Consuming message from user: {message.from_user.username}')
+
+        @self.bot.message_handler(content_types=['text'])
+        def handle_joke_themes(message):
+            if message.text in self.themes.keys():
+                joke = self.get_fun_content(self.themes.get(message.text))
+                self.bot.send_message(message.chat.id, joke)
 
         self.logger.info('Setup handlers complete')
 
     def run(self, *args, **kwargs):
         self.setup_handlers()
-        self.bot.polling(*args, **kwargs)
+        try:
+            self.bot.polling(*args, **kwargs)
+        except requests.exceptions.ProxyError as e:
+            self.run()
+
 
     def get_fun_content(self, theme: int) -> str:
         """
@@ -79,6 +92,7 @@ class JokeBot:
         16 - Тосты (+18);
         18 - Статусы (+18);
         """
+        self.logger.info('Receive joke from external server')
         api_url = f'http://rzhunemogu.ru/RandJSON.aspx?CType={theme}'
         content = requests.get(api_url).content.decode('windows-1251')
         self.logger.debug(content)
