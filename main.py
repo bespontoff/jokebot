d325e9c7da5617ec9bec73d9bdec9d9f6850e87e
bespontoff <bespontoff@gmail.com>
2020-06-01 21:54:24 +0300

    добавлен лок и основной класс

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'bespontoff'
 
+import telebot
+
+token = '237625759:AAFAgX-oY_lelc1X27PYRZFw0dCykfdR_Qk'
+
+
+class JokeBot:
+    def __init__(self):
+        self.bot = telebot.TeleBot(token)
+
+
+if __name__ == '__main__':
+    jb = JokeBot()
+    jb.bot.get_me()
